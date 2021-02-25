72e381d3e9d37adf3e6de85bb2a0baebbe7e90ff
XiEn1847 <79648987+XiEn1847@users.noreply.github.com>
2021-02-25 22:32:31 +0800

    Add files via upload
    updated with a better view

--- checker.cpp
+++ checker.cpp
@@ -1,10 +1,13 @@
 #include<bits/stdc++.h>
-/*  */
 #include<windows.h>
 using namespace std;
-char tmp[1000],nme[1000],tnme[1000];
-void color(short x){
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x);
+
+struct status{
+	int stat;	//0=WA 1=AC 2=TLE
+};
+
+inline void color(short x){
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x);
 }
 void cac(int ac,int al){
 	double perc=ac*100.0/al;
@@ -13,27 +16,63 @@ void cac(int ac,int al){
 	if(40<perc&&perc<80)color(14);
 }
 int main(){
-	SYSTEMTIME STime1,STime2;
-	int cnt;
-	int cAC=0;
-	int cMS=0,cS=0,cMIN=0,cH=0;
-	int _h,_min,_sec,_ms;
 	
-	cerr<<"Please Enter The Name Of Your Program.\n\n e.g. sequence (if you're using sequence.exe)\n\n>>";
-	scanf("%s",tnme);
-	cerr<<"Please Enter The Range Of Data.\n\n e.g. 10 (if you're using 1.in, 2.in, ..., 10.in)\n\n>>";
-	scanf("%d",&cnt);
+	//////	Definition
+	int cnt,cAC;
+	int cMS,cS,cMIN,cH;
+	int _h,_min,_sec,_ms;
+	char tmp[1000],nme[1000],tnme[1000],ext[1000];
+	SYSTEMTIME STime1,STime2;
+	bool isinname;
+	
+	//////	Initialization
+	memset(tmp,0,sizeof(tmp));
+	memset(tmp,0,sizeof(nme));
+	memset(tmp,0,sizeof(tnme));
+	
+	cnt=0,cAC=0;
+	cMS=0,cS=0,cMIN=0,cH=0;
+	_h=0,_min=0,_sec=0,_ms=0;
+	
+	isinname=false;
+	//	true:  abcd1.in, abcd2.in, ...
+	//	false: 1.in, 2.in, ...
+	//////	
+	color(7);
+	cerr<<"Please Enter The Name Of Your Program.\n\ne.g. ";
+	color(14);cerr<<"sequence";color(7);
+	cerr<<" (using sequence.exe)\n\n>> ";
+	color(14);scanf("%s",tnme);color(7);
+	
+	cerr<<"Please Enter The Range Of The Standard Input.\n\ne.g. ";
+	color(14);cerr<<"10";color(7);
+	cerr<<" (using 1.in, 2.in, ..., 10.in)\n\n>> ";
+	color(14);scanf("%d",&cnt);color(7);
+	
+	cerr<<"Please Enter The Extension Name Of The Standard Output.\n\ne.g. ";
+	color(14);cerr<<"ans";color(7);
+	cerr<<" (using 1.ans, 2.ans, ..., 10.ans as the correct answer)\n\n>> ";
+	color(14);scanf("%s",ext);color(7);
 	
 	for(int i=1;i<=cnt;i++){
 		
-		strcpy(nme,tnme);	// int 123-> string "123"
-		stringstream s;
+		strcpy(nme,tnme);
+		
+		stringstream s;				// int 123-> string "123"
 		s.str("");s<<i;s>>tmp;
 		
-		strcat(nme,".exe <");strcat(nme,tmp);strcat(nme,".in >");strcat(nme,tmp);strcat(nme,".out");	// *.exe <#.in >#.out
-		GetLocalTime(&STime1);system(nme);GetLocalTime(&STime2);
-		strcpy(nme,"fc ");strcat(nme,tmp);strcat(nme,".out ");strcat(nme,tmp);strcat(nme,".ans");		// fc *.out *.ans
 		
+	//	if(isinname){strcat(nme,tnme);}
+		
+		strcat(nme,".exe <");if(isinname){strcat(nme,tnme);}strcat(nme,tmp);strcat(nme,".in >");if(isinname){strcat(nme,tnme);}strcat(nme,tmp);strcat(nme,".JudgeOut ");	// *.exe <#.in >#.JudgeOut
+		
+		GetLocalTime(&STime1);
+		
+		system(nme);
+		
+		GetLocalTime(&STime2);//cout<<nme;
+		
+		strcpy(nme,"fc ");if(isinname){strcat(nme,tnme);}strcat(nme,tmp);strcat(nme,".JudgeOut ");if(isinname){strcat(nme,tnme);}strcat(nme,tmp);strcat(nme,".");strcat(nme,ext);		// fc *.JudgeOut *.xxx	
 		_h=STime2.wHour-STime1.wHour;
 		_min=STime2.wMinute-STime1.wMinute;
 		_sec=STime2.wSecond-STime1.wSecond;
@@ -41,28 +80,41 @@ int main(){
 		if(_ms<0)_sec--,_ms+=1000;
 		if(_sec<0)_min--,_sec+=60;
 		if(_min<0)_h--,_min+=60;
-			
+		
+		
 		if(!system(nme)){
 			if(_h==0&&_min==0&&_sec==0){
-				color(10);cerr<<" AC";
-				color(7);cerr<<" with ";
 				cAC++;
-				cerr<<_h<<" h "<<_min<<" min "<<_sec<<" s "<<_ms<<" ms\n";	
+				
+				color(10);	cerr<<" AC      ";
+				color(7);	cerr<<" with "<<_h<<" h "<<_min<<" min "<<_sec<<" s "<<_ms<<" ms\n";	
 				cH+=_h;	cMIN+=_min;	cS+=_sec;	cMS+=_ms;
 				if(cMS>=1000){cS+=cMS/1000;cMS%=1000;};	if(cS>=60){cMIN+=cS/60;cS%=60;};	if(cMIN>=60){cH+=cMIN/60;cMIN%=60;};
 			}else{
-				color(9);cerr<<" TLE";
-				color(7);cerr<<" with ";
-				cerr<<_h<<" h "<<_min<<" min "<<_sec<<" s "<<_ms<<" ms\n";
+				color(9);	cerr<<" TLE ";
+				color(10);	cerr<<"(AC)";
+				color(9);	cerr<<"";
+				color(7);	cerr<<" with "<<_h<<" h "<<_min<<" min "<<_sec<<" s "<<_ms<<" ms\n";
 			}
 		}else{
-			color(12);cerr<<" WA";
-			color(7);cerr<<" with ";
-			cerr<<_h<<" h "<<_min<<" min "<<_sec<<" s "<<_ms<<" ms\n";
+			if(_h==0&&_min==0&&_sec==0){
+				color(12);	cerr<<" WA      ";
+				color(7);	cerr<<" with "<<_h<<" h "<<_min<<" min "<<_sec<<" s "<<_ms<<" ms\n";
+			}else{
+				color(9);	cerr<<" TLE ";
+				color(12);	cerr<<"(WA)";
+				color(9);	cerr<<"";
+				color(7);	cerr<<" with "<<_h<<" h "<<_min<<" min "<<_sec<<" s "<<_ms<<" ms\n";
+			}
 		}
 	}
+	
+	
 	cerr<<"\n\nTest Finished.\n\n"<<" AC: ";
 	cac(cAC,cnt);cerr<<cAC<<"/"<<cnt;color(7);
-	cerr<<"    in "<<cH<<" h "<<cMIN<<" min "<<cS<<" s "<<cMS<<" ms\n"<<"\nPress Enter To Exit . . . \n";
-	while(1);
+	cerr<<"    in "<<cH<<" h "<<cMIN<<" min "<<cS<<" s "<<cMS<<" ms\n"<<"\nEnter \"0\" To Exit . . . \n";
+	
+	
+	while(getchar()!='0');
+	return 0;
 }
